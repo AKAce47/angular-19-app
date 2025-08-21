23b25aa6eb37fa99d827c5e9e651a43d7ebf8d45
AKAce47 <anrich.kok@gmail.com>
2025-08-21 21:41:19 +0200

    Added nav properties for login and logout capabilities

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'layout-header',
@@ -9,5 +10,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
 
+  protected isLoggedIn = computed(() => this.authService.authenticated());
+
+  protected profile(): void {
+    this.router.navigate(['/account/profile']);
+  }
+
+  protected login(): void {
+    this.router.navigate(['/account/login']);
+  }
+
+  protected logout(): void {
+    this.authService.logOut();
+    this.router.navigate(['/']);
+  }
 }
